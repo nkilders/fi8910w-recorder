3e9908185281e2892922cb370859a78f648ce363
nkilders <71071641+nkilders@users.noreply.github.com>
2024-01-05 11:05:46 +0100

    Format code

--- src/main/java/de/nkilders/FI8910WRecorderException.java
+++ src/main/java/de/nkilders/FI8910WRecorderException.java
@@ -2,12 +2,12 @@ package de.nkilders;
 
 public class FI8910WRecorderException extends Exception {
 
-    public FI8910WRecorderException(String message) {
-        this(message, null);
-    }
+	public FI8910WRecorderException(String message) {
+		this(message, null);
+	}
 
-    public FI8910WRecorderException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
+	public FI8910WRecorderException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
 
 }
